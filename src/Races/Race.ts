022b8a319b69d2d949ef3c286eb247afabeedf94
Diego Siqueira <diego.camomila@gmail.com>
2022-08-10 15:04:23 -0300

    docs: comandos para usar com docker

---
+++ src/Races/Race.ts
@@ -0,0 +1,29 @@
+// public: permite acesso a qualquer código externo a classe.
+// protected: permite acesso às classes filhas, mas proíbe a qualquer outro acesso externo.
+// private: proíbe qualquer acesso externo à própria classe, inclusive das classes filhas.
+
+export default abstract class Race {
+  protected _maxLifePoints = 0;
+  protected _name:string;
+  protected _dexterity:number;
+
+  constructor(name:string, dexterity:number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+  
+  static createdRacesInstances():number {
+    throw new Error('Not implemented');
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get dexterity() {
+    return this._dexterity;
+  }
+
+  abstract get maxLifePoints():number;
+  abstract set maxLifePoints(number);
+}

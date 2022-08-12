f18f4eae3e16835f286850dafbcf49b232864987
Diego Siqueira <diego.camomila@gmail.com>
2022-08-12 14:27:37 -0300

    feat: requisito 13, Crie objetos no arquivo index

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import Battle, { PVE, PVP } from './Battle';
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
+
+const player1 = new Character('player1');
+const player2 = new Character('player2');
+const player3 = new Character('player3');
+
+// Execute algumas vezes o método levelUp do player1
+for (let index = 0; index < 10; index += 1) {
+  player1.levelUp();
+}
+
+const monster1 = new Monster();
+const monster2 = new Dragon();
+
+const pvp = new PVP(player2, player3);
+const pve = new PVE(player1, [monster1, monster2]);
+
+function runBattles(battles: Battle[]): void {
+  for (let index = 0; index < battles.length; index += 1) {
+    battles[index].fight();
+  }
+}
+
+export { runBattles };
+
+export {
+  player1,
+  player2,
+  player3,
+  monster1,
+  monster2,
+  pvp,
+  pve,
+};
